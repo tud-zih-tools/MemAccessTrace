079dff519625e4740c082f8347b4ded6200d8ae4
harryherold <harryherold@googlemail.com>
2019-03-28 10:16:32 +0100

    Implement bindings for enums and AccessEvent.

--- src/pybind_trace_file.cpp
+++ src/pybind_trace_file.cpp
@@ -1,10 +1,59 @@
+#include <sstream>
+
 #include <pybind11/pybind11.h>
 #include <pybind11/stl_bind.h>
 
 #include <trace_events.h>
 #include <trace_file.h>
 
+namespace py = pybind11;
+
 PYBIND11_MODULE(tracefile, m)
 {
-    m.doc() = "Read/write trace files for sampled memory accesses";
+    m.doc() = "The module provides an API to write recorded memory accesses to a trace file."
+              "tracefiles provides also classes for storing access events and their relevant information.";
+
+    py::enum_<AccessType>(m, "AccessType")
+        .value("LOAD", AccessType::LOAD)
+        .value("STORE", AccessType::STORE)
+        .value("PREFETCH", AccessType::PREFETCH)
+        .value("EXEC", AccessType::EXEC)
+        .value("NA", AccessType::NA);
+
+    py::enum_<MemoryLevel>(m, "MemoryLevel")
+        .value("MEM_LVL_NA", MemoryLevel::MEM_LVL_NA)
+        .value("MEM_LVL_HIT", MemoryLevel::MEM_LVL_HIT)
+        .value("MEM_LVL_MISS", MemoryLevel::MEM_LVL_MISS)
+        .value("MEM_LVL_L1", MemoryLevel::MEM_LVL_L1)
+        .value("MEM_LVL_L2", MemoryLevel::MEM_LVL_L2)
+        .value("MEM_LVL_L3", MemoryLevel::MEM_LVL_L3)
+        .value("MEM_LVL_LFB", MemoryLevel::MEM_LVL_LFB)
+        .value("MEM_LVL_LOC_RAM", MemoryLevel::MEM_LVL_LOC_RAM)
+        .value("MEM_LVL_REM_RAM1", MemoryLevel::MEM_LVL_REM_RAM1)
+        .value("MEM_LVL_REM_RAM2", MemoryLevel::MEM_LVL_REM_RAM2)
+        .value("MEM_LVL_REM_CCE1", MemoryLevel::MEM_LVL_REM_CCE1)
+        .value("MEM_LVL_REM_CCE2", MemoryLevel::MEM_LVL_REM_CCE2)
+        .value("MEM_LVL_IO", MemoryLevel::MEM_LVL_IO)
+        .value("MEM_LVL_UNC", MemoryLevel::MEM_LVL_UNC);
+
+    py::class_<AccessEvent>(m, "AccessEvent")
+        .def(py::init<uint64_t, uint64_t, uint64_t, AccessType, MemoryLevel>(),
+             py::arg("timestamp") = 0, py::arg("address") = 0, py::arg("ip") = 0,
+             py::arg("type") = AccessType::NA, py::arg("level") = MemoryLevel::MEM_LVL_NA)
+        .def_readonly("timestamp", &AccessEvent::time)
+        .def_readonly("address", &AccessEvent::address)
+        .def_readonly("ip", &AccessEvent::ip)
+        .def_readonly("type", &AccessEvent::access_type)
+        .def_readonly("level", &AccessEvent::memory_level)
+        .def("__str__",
+             [](const AccessEvent& a)
+             {
+                 std::stringstream ss;
+                 ss << "[Timestamp: " << a.time << ", "
+                    << "Address: " << std::hex << a.address << ", "
+                    << "IP: " << std::hex << a.ip << ", "
+                    << "Type: " << toString(a.access_type) << ", "
+                    << "Level: " << toString(a.memory_level) << "]";
+                return ss.str();
+             });
 }
--- test/test_trace_file.py
+++ test/test_trace_file.py
@@ -1,5 +1,21 @@
 #! /usr/bin/env python3
+import unittest
+import tracefile as tf
 
-import tracefile
+class TestAccessEvent(unittest.TestCase):
+    def test_creation(self):
+        a = tf.AccessEvent(1, 1, 42, tf.AccessType.LOAD, tf.MemoryLevel.MEM_LVL_L1)
+        self.assertEqual(a.timestamp, 1)
+        self.assertEqual(a.address, 1)
+        self.assertEqual(a.ip, 42)
+        self.assertEqual(a.type, tf.AccessType.LOAD)
+        self.assertEqual(a.level, tf.MemoryLevel.MEM_LVL_L1)
+        b = tf.AccessEvent()
+        self.assertEqual(b.timestamp, 0)
+        self.assertEqual(b.address, 0)
+        self.assertEqual(b.ip, 0)
+        self.assertEqual(b.type, tf.AccessType.NA)
+        self.assertEqual(b.level, tf.MemoryLevel.MEM_LVL_NA)
 
-help(tracefile)
+if __name__ == '__main__':
+    unittest.main()
